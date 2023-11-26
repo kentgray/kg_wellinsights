09c6354b15317d1ffacb48aad9021c14f831df34
kegray <kegray@sofi.org>
2023-11-26 09:52:58 -0500

    forecast table and validation queries

--- models/fact/fact_forecast.sql
+++ models/fact/fact_forecast.sql
@@ -8,27 +8,51 @@ WITH economic_data AS (
         day_init,
         gas_rate_init,
         gas_b_factor,
+        generate_series(day_init, '2023-12-31'::date, '1 day') AS forecast_date,
         gas_decline_init_daily_nom
     FROM {{ ref('stg__aries__ac_economic') }}
 ),
 
-dim_date AS (
+
+date_series AS (
+    SELECT 
+        propnum,
+        day_init,
+        gas_rate_init,
+        gas_b_factor,
+        gas_decline_init_daily_nom,
+        generate_series(day_init, '2023-12-31'::date, '1 day') AS forecast_date
+    FROM economic_data
+),
+
+forecast_calc AS (
     SELECT
-        date_id,
-        date
-    FROM {{ ref('dim_date') }}
+        ds.propnum,
+        ds.forecast_date,
+        ds.gas_rate_init::numeric / (1 + (ds.gas_b_factor::numeric * ds.gas_decline_init_daily_nom::numeric * EXTRACT(EPOCH FROM (ds.forecast_date - ds.day_init)) / 86400))::numeric AS gas_forecast_volume
+    FROM date_series ds
 )
 
 SELECT
-    ROW_NUMBER() OVER () AS forecast_id,
-    e.propnum::UUID as well_id,
-    d.date_id,
-    CASE
-        WHEN d.date >= e.day_init THEN
-            e.gas_rate_init::numeric / (1 + (e.gas_b_factor::numeric * e.gas_decline_init_daily_nom::numeric * EXTRACT(EPOCH FROM (d.date - e.day_init)) / 86400))
-        ELSE
-            NULL
-    END AS gas_forecast_volume
-FROM    economic_data e
-JOIN    dim_date d  ON d.date = e.day_init::date
---  e.day_init AND '2023-12-31'::date
+    ROW_NUMBER() OVER (ORDER BY fc.propnum, fc.forecast_date) as forecast_id,
+    fc.propnum::UUID AS well_id,
+    dd.date_id,
+    fc.gas_forecast_volume
+FROM
+    forecast_calc fc
+JOIN {{ ref('dim_date') }} dd ON fc.forecast_date = dd.date
+-- SELECT
+--     ROW_NUMBER() OVER () AS forecast_id,
+--     e.propnum::UUID as well_id,
+--     d.date_id,
+--     CASE
+--         WHEN d.date >= e.day_init THEN
+--             e.gas_rate_init::numeric / (1 + (e.gas_b_factor::numeric * e.gas_decline_init_daily_nom::numeric * EXTRACT(EPOCH FROM (d.date - e.day_init)) / 86400))
+--         ELSE
+--             NULL
+--     END AS gas_forecast_volume
+-- FROM    economic_data e
+-- JOIN    dim_date d  ON d.date = e.day_init::date
+-- --  e.day_init AND '2023-12-31'::date
+
+
---
+++ validation_queries.sql
@@ -0,0 +1,64 @@
+set search_path to kg_wellinsights;
+
+-- 1. Number of Wells per Operator
+SELECT
+    operator,
+    COUNT(DISTINCT well_id) AS number_of_wells
+FROM    dim_well
+GROUP BY    operator;
+
+-- 2. Total Lost Gas Production for Well with API = 0000000485 in 2020
+-- Assuming the API number is a field in dim_well:
+SELECT    SUM(ABS(forecast.gas_forecast_volume - production.gas_volume)) AS total_lost_gas
+FROM    fact_forecast forecast
+        JOIN    fact_production production ON forecast.well_id = production.well_id AND forecast.date_id = production.date_id
+        JOIN    dim_date d ON production.date_id = d.date_id
+        JOIN    dim_well w ON forecast.well_id = w.well_id
+WHERE        w.apiwellnumber = '0000000485' AND EXTRACT(YEAR FROM d.date) = 2020;
+
+-- 3. Total Lost Gas Production for All Wells, Grouped by Area, for 2021
+-- Assuming area is a field in dim_well:
+SELECT
+    w.area,
+    SUM(ABS(forecast.gas_forecast_volume - production.gas_volume)) AS total_lost_gas
+FROM    fact_forecast forecast
+        JOIN    fact_production production ON forecast.well_id = production.well_id AND forecast.date_id = production.date_id
+        JOIN    dim_date d ON production.date_id = d.date_id
+        JOIN    dim_well w ON forecast.well_id = w.well_id
+WHERE        EXTRACT(YEAR FROM d.date) = 2021
+GROUP BY    w.area;
+
+-- 4. Pad with Most Net Gas on 07/14/2022
+-- Assuming padname and nri (Net Revenue Interest) are fields in dim_well:
+SELECT
+    w.padname,
+    SUM(production.gas_volume * w.nri) AS net_gas
+FROM    fact_production production
+        JOIN    dim_date d ON production.date_id = d.date_id
+        JOIN    dim_well w ON production.well_id = w.well_id
+WHERE        d.date = '2022-07-14'
+GROUP BY    w.padname
+ORDER BY    net_gas DESC
+LIMIT 1;
+
+-- 5. Total Cost of Workover Events by Field Group for June 2022
+-- Assuming fieldgroup is a field in dim_well:
+SELECT
+    w.fieldgroup,
+    SUM(workover.total_cost) AS total_cost
+FROM    fact_workover workover
+        JOIN    dim_date start_date ON workover.start_date_id = start_date.date_id
+        JOIN    dim_date end_date ON workover.end_date_id = end_date.date_id
+        JOIN    dim_well w ON workover.well_id = w.well_id
+WHERE        EXTRACT(MONTH FROM start_date.date) = 6 AND EXTRACT(YEAR FROM start_date.date) = 2022
+GROUP BY    w.fieldgroup;
+
+-- 6. Wells with Highest Deviation of Gas from Forecast in July 2023
+SELECT
+    forecast.well_id,
+    ABS(forecast.gas_forecast_volume - production.gas_volume) AS deviation
+FROM    fact_forecast forecast
+        JOIN    fact_production production ON forecast.well_id = production.well_id AND forecast.date_id = production.date_id
+        JOIN    dim_date d ON production.date_id = d.date_id
+WHERE        EXTRACT(MONTH FROM d.date) = 7 AND EXTRACT(YEAR FROM d.date) = 2023
+ORDER BY    deviation DESC;
